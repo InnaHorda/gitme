ec7f06c749d7020806ef0c4ee41bb3b068a5669b
InnaHorda <inna.horda@gmail.com>
2022-07-10 23:02:20 +0300

    4444

--- 12333/12333/Program.cs
+++ 12333/12333/Program.cs
@@ -6,7 +6,7 @@ namespace _12333
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!gfgfg");
         }
     }
 }
